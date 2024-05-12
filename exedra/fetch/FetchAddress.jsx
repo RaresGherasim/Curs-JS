bc7341068ee8a6b15efe821fc4e8781e30024dd6
Gherasim Rares <rares.gherasim@gmail.com>
2024-05-12 12:38:17 +0300

    fixed the reverse gocoding issues

--- exedra/fetch/FetchAddress.jsx
+++ exedra/fetch/FetchAddress.jsx
@@ -1,11 +1,11 @@
 export default async function getAddress(lat, long, key){
     const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&location_type=ROOFTOP&result_type=street_address&key=${key}`, {
         method: "GET", // *GET, POST, PUT, DELETE, etc.
-        mode: "cors",
         headers: {
         //    "Authorization": `Bearer ${requestToken}`,
-          "Content-Type": "application/json",
+        //   "Content-Type": "application/json",
           "Accept": "application/json",
+        //   "credentials": 'include'
         //   'Cache-Control': 'no-cache',
         //   'exedra-subscription-key': 'ce41a2f87bdd493fb4734425d6fd9c03'
 
@@ -18,6 +18,6 @@ export default async function getAddress(lat, long, key){
       let addressData = await response;
       console.log(addressData);
       let data = await addressData.json();
-      console.log(data);
+      console.log(data.results[0].formatted_address);
 }
 
